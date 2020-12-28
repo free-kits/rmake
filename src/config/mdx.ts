0ef40e0dbf350da7c5082fd3941a2e5990f4b947
jhoneybee <zhangjin199600908@gmail.com>
2020-12-28 12:53:09 +0800

    fix: 消除重复代码

--- src/config/mdx.ts
+++ src/config/mdx.ts
@@ -1,49 +1,40 @@
-import Config from 'webpack-chain';
+import Config, { Use } from 'webpack-chain';
 import { getConfig } from './setting';
+
+const presetBabel = (use: Use, targets: string) => {
+    use.loader('babel-loader')
+        .options({
+            presets: [
+                [
+                    '@babel/preset-env',
+                    {
+                        targets,
+                    },
+                ],
+                '@babel/preset-react',
+                '@babel/preset-typescript',
+            ],
+        })
+        .end();
+};
 
 export default (config: Config) => {
     const docConfig = getConfig();
     const targets = docConfig.targets || 'defaults';
-    config.module
+
+    presetBabel(config.module
         .rule('babel-mdx')
         .test(/\.md(x?)$/)
         .use('babel-mdx/loader')
         .loader('@mdx-js/loader')
         .end()
-        .use('babel-mdx/loader')
-        .loader('babel-loader')
-        .options({
-            presets: [
-                [
-                    '@babel/preset-env',
-                    {
-                        targets,
-                    },
-                ],
-                '@babel/preset-react',
-                '@babel/preset-typescript',
-            ],
-        })
-        .end();
+        .use('babel-mdx/loader'), targets);
 
-    config.module
+    presetBabel(config.module
         .rule('babel-react')
         .test(/\.ts(x?)$/)
         .use('babel-react/loader')
-        .loader('babel-loader')
-        .options({
-            presets: [
-                [
-                    '@babel/preset-env',
-                    {
-                        targets,
-                    },
-                ],
-                '@babel/preset-react',
-                '@babel/preset-typescript',
-            ],
-        })
-        .end();
+        .loader('babel-loader'), targets);
 
     config.module
         .rule('css')
