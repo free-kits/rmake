f5e0abc3872761c653bce96041b64faaf7105c32
jhoneybee <zhangjin199600908@gmail.com>
2020-12-28 10:28:11 +0800

    feat: 添加开发的时候，可以自动编译，减少启动次数

--- template/webpack/.doc/components/layout/doc-layout.tsx
+++ template/webpack/.doc/components/layout/doc-layout.tsx
@@ -4,18 +4,18 @@ import './style/doc-layout.less';
 
 export const DocLayout = () => {
     return (
-        <>
+        <div className={`${prefixCls}-layout`}>
             <header className={`${prefixCls}-header`}></header>
             <div className={`${prefixCls}-body`}>
                 <div className={`${prefixCls}-body-sider`}>
                     <ul>
                         <li>咖啡</li>
-                        <li>茶</li>
+                        <li>茶222</li>
                         <li>牛奶</li>
                     </ul>
                 </div>
                 <div className={`${prefixCls}-body-content`}></div>
             </div>
-        </>
+        </div>
     );
 };
