c293f0ead1cea19a0de15533b17b4da175afb8f6
jhoneybee <zhangjin199600908@gmail.com>
2020-12-30 10:03:50 +0800

    feat: 添加测试用例

--- src/config/__tests__/jsyaml.test.ts
+++ src/config/__tests__/jsyaml.test.ts
@@ -10,9 +10,5 @@ group:
     title: 基础组件
 title: Button 按钮`;
     const data: any = yaml.safeLoad(testData);
-    expect(data.nav.title).toBe('组件');
-    expect(data.nav.path).toBe('/components');
-    expect(data.group.path).toBe('/components/base');
-    expect(data.group.title).toBe('基础组件');
-    expect(data.title).toBe('Button 按钮');
+    expect(data).toMatchSnapshot();
 });
