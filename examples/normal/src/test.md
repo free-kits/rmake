79ce6098ddf52dcf9a4e259dcb38f7c3d99ff200
jhoneybee <zhangjin199600908@gmail.com>
2020-12-30 15:12:44 +0800

    feat: 生成对应的配置文件信息

--- examples/normal/src/test.md
+++ examples/normal/src/test.md
@@ -1,9 +1,9 @@
----
+<!---
 nav:
     title: 组件
 group:
     title: 基础组件
 title: Button 按钮
----
+--->
 
 # 这是一个测试的MD文件
--- src/config/router.ts
+++ src/config/router.ts
@@ -1,4 +1,6 @@
-import { readdirSync, readFileSync, writeFileSync } from 'fs-extra';
+import {
+    readdirSync, readFileSync, writeFileSync, statSync, Stats,
+} from 'fs-extra';
 import yaml from 'js-yaml';
 import { join } from 'path';
 
@@ -25,20 +27,43 @@ interface Nav {
     groups: Group[]
 }
 
+// 递归获取文件目录
+export const findRecursionFiles = (path: string, callback: (file: string, stats:Stats) => void) => {
+    const dir = readdirSync(path);
+    dir.forEach((file) => {
+        if (['node_modules'].includes(file)) {
+            return;
+        }
+        const realFile = join(path, file);
+        logger.debug(realFile);
+        const stat = statSync(realFile);
+        if (stat.isDirectory()) {
+            findRecursionFiles(realFile, callback);
+        } else {
+            callback(realFile, stat);
+        }
+    });
+};
+
 // 查找对应的md的文件, 然后根据文件来生成对应的目录结构信息, 应该采用递归的方式进行获取
 export const findFile = () => {
-    const dir = readdirSync(process.cwd());
-    const mdFiles = dir.filter((file) => file.match(/\.md(x?)$/));
+    const mdFiles: string[] = [];
+    findRecursionFiles(process.cwd(), (path) => {
+        if (path.match(/\.md(x?)$/)) {
+            mdFiles.push(path);
+        }
+    });
     return mdFiles;
 };
 
 // 找到当前的路由信息，并生成navs信息
 export const findFileToNavs = () => {
     const files = findFile();
-    logger.info('find md files.', files);
+    logger.debug('find md files.', files);
     const navs: Nav[] = [];
     files.forEach((file) => {
-        const mdInfo = /^---[\W\w]+?---/i.exec(readFileSync(file, 'utf8'));
+        const mdInfo = /^<!--+[\W\w]+?--+>/i.exec(readFileSync(file, 'utf8'));
+        logger.debug('get markdown params', mdInfo);
         if (mdInfo?.length === 1) {
             /**
              * 数据格式如下
@@ -48,7 +73,7 @@ export const findFileToNavs = () => {
              *  title: 基础组件
              * title: Button 按钮`;
              */
-            const yml: any = yaml.safeLoad(mdInfo[0]);
+            const yml: any = yaml.safeLoad(mdInfo[0].replace(/<!--+/g, '').replace(/--+>/g, ''));
             const filterNav = navs.filter((ele) => ele.title === yml.nav.title);
             // 不存在对应的nav信息，则添加
             const groupInfo = {
@@ -56,7 +81,7 @@ export const findFileToNavs = () => {
                 path: yml.group.path as string,
                 pages: [{
                     title: yml.title as string,
-                    path: `/*@freekits/doc import*/ ${file.replace(process.cwd(), '')}`,
+                    path: `/*@freekits/doc import*/..${file.replace(process.cwd(), '')}/*@freekits/doc import-end*/`,
                 }],
             };
             if (navs.filter((ele) => ele.title === yml.nav.title).length === 0) {
@@ -80,5 +105,10 @@ export const findFileToNavs = () => {
 // 生成对应的文件信息
 export const createRouteConfig = () => {
     const navs = findFileToNavs();
-    writeFileSync(join(process.cwd(), '.doc', 'config.ts'), `export default () => ${JSON.stringify(navs)}`);
+    logger.debug('create route config ->', navs);
+    const code = JSON.stringify(navs).replace(/"/g, '\'')
+        .replace(/'\/\*@freekits\/doc\s+import\*\//g, 'React.lazy(() => import(\'')
+        .replace(/\/\*@freekits\/doc\s+import-end\*\/'/g, '\'))')
+        .replace(/\s*\\\\/g, '/');
+    writeFileSync(join(process.cwd(), '.doc', 'config.ts'), `import React from 'react';\nexport default () => ${code}`);
 };
