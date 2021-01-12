8252f62c538a39f7052171f8ba01de6ce39028c4
jhoneybee <zhangjin199600908@gmail.com>
2021-01-12 14:02:08 +0800

    chore: 修复React的一些常见问题

--- src/config/plugin.ts
+++ src/config/plugin.ts
@@ -1,29 +1,11 @@
 import Config from 'webpack-chain';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { join } from 'path';
 
 export default (config: Config) => {
     config.plugin('html-webpack-plugin').use(
         new HtmlWebpackPlugin({
-            templateContent: `<!DOCTYPE html>
-<html>
-    <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-    />
-    <style>
-        html,body, #root {
-        margin: 0px;
-        height: 100%;
-        }
-    </style>
-    </head>
-    <body>
-    <div id="root"></div>
-    </body>
-</html>`,
+            template: join(process.cwd(), '.doc', 'pages', 'document.ejs'),
         }),
     );
 };
