06c969aa4d7348fe2c4b7cf8f9beeecfd8a45f8d
jhoneybee <zhangjin199600908@gmail.com>
2021-01-15 09:20:43 +0800

    feat: 添加站点的预览，以及markdown的样式

---
+++ template/webpack/.doc/components/markdown/blockquote.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/blockquote.less'
+
+export default ({
+    children
+}) => {
+    return (
+        <blockquote className={'md-blockquote'}>
+            {children}
+        </blockquote>
+    )
+}
--- template/webpack/.doc/components/markdown/code.tsx
+++ template/webpack/.doc/components/markdown/code.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
-import theme from 'prism-react-renderer/themes/github';
+import theme from 'prism-react-renderer/themes/duotoneLight';
+import './styles/code.less';
 
 export default ({ children, className }) => {
     const language = className.replace(/language-/, '');
+    console.log(language)
     return (
         <Highlight
             {...defaultProps}
@@ -14,7 +16,7 @@ export default ({ children, className }) => {
             {({ className, style, tokens, getLineProps, getTokenProps }) => (
                 <pre
                     className={className}
-                    style={{ ...style, padding: '.8rem' }}
+                    style={style}
                 >
                     {tokens.map((line, i) => (
                         <div key={i} {...getLineProps({ line, key: i })}>
