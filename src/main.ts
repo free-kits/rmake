cfc220b949d9526184b6c29f040aed358b556562
jhoneybee <zhangjin199600908@gmail.com>
2021-01-25 17:03:12 +0800

    feat: 可使用CLI命令来创建模板项目

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,50 @@
 
 import chokidar from 'chokidar';
 import { join } from 'path';
+import inquirer from 'inquirer';
+import handlebars from 'handlebars';
+import { copySync, readFileSync, writeFileSync } from 'fs-extra';
+
+import validateNpmName from 'validate-npm-package-name';
 import { compiler, devServer, copyFileDoc } from './config/server';
+
+// 创建项目
+if (process.argv.length === 2) {
+    (async () => {
+        // 获得用户输入的信息
+        const info = await inquirer.prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'Package Name',
+            validate: (name) => name && validateNpmName(name).validForNewPackages
+        }, {
+            type: 'input',
+            name: 'title',
+            message: 'Project title',
+        }, {
+            type: 'input',
+            name: 'description',
+            message: 'Package Description',
+        }, {
+            type: 'input',
+            name: 'license',
+            message: 'License',
+            default: 'MIT',
+        }]);
+
+        // 复制文件到指定目录
+        copySync(
+            join(__dirname, '..', 'template', 'cli'),
+            process.cwd(),
+        );
+        const targetPackage = join(process.cwd(), 'package.json');
+        const template = handlebars.compile(readFileSync(targetPackage, 'utf8'));
+        const content = template({
+            ...info,
+        });
+        writeFileSync(targetPackage, content, 'utf8');
+    })();
+}
 
 // 校验当前的参数是否正确
 if (
