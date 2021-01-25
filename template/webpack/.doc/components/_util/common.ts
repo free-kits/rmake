9e671180de2e5cc947d3c57b544fbf6ca1e1bade
jhoneybee <zhangjin199600908@gmail.com>
2021-01-25 16:07:48 +0800

    feat: 添加nav和side的排序功能

--- template/webpack/.doc/components/_util/common.ts
+++ template/webpack/.doc/components/_util/common.ts
@@ -1,1 +1,15 @@
 export const prefixCls = 'free-kits-doc';
+
+export const sort = (data) => {
+    return data?.sort((current, next) => {
+        const currentOrder = current.order || 0;
+        const nextOrder = next.order || 0;
+        if (currentOrder > nextOrder) {
+            return 1;
+        }
+        if (currentOrder === nextOrder) {
+            return 0;
+        }
+        return -1;
+    })
+}
