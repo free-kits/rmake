e126df433dff39cc64162524b0e8f7ca83f205b4
jhoneybee <zhangjin199600908@gmail.com>
2021-01-28 15:51:00 +0800

    feat: 可以在线编辑和预览React组件, 通过特定方式引入当前项目开发的组件库

--- template/webpack/.doc/components/markdown/code.tsx
+++ template/webpack/.doc/components/markdown/code.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
-import theme from 'prism-react-renderer/themes/duotoneLight';
+import theme from 'prism-react-renderer/themes/vsDark';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
+
 import './styles/code.less';
 
-export default ({ children, className }) => {
+export default ({ children, className, live }) => {
     const language = className?.replace(/language-/, '') || 'shell';
+    if (live) {
+        return (
+            <div style={{ marginTop: '40px' }}>
+                <LiveProvider code={children.trim()} noInline theme={theme}>
+                    <LivePreview />
+                    <LiveEditor className='live-editor' />
+                    <LiveError />
+                </LiveProvider>
+            </div>
+        );
+    }
+
     return (
         <Highlight
             {...defaultProps}
@@ -13,10 +27,7 @@ export default ({ children, className }) => {
             language={language}
         >
             {({ className, style, tokens, getLineProps, getTokenProps }) => (
-                <pre
-                    className={className}
-                    style={style}
-                >
+                <pre className={className} style={style}>
                     {tokens.map((line, i) => (
                         <div key={i} {...getLineProps({ line, key: i })}>
                             {line.map((token, key) => (
